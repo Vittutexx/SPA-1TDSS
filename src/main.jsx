1f8860cca27d17505f8e1311d0a48d002d9355a5
Vittutexx <vitortex06@gmail.com>
2023-09-15 14:21:31 -0300

    backup do exercicio 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,29 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
-//BLOCO DE ROTAS
+//Bloco de rotas
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import EditarProdutos from './routes/EditarProdutos.jsx';
-import Erro404 from './routes/Erro404.jsx';
-import Home from './routes/Home.jsx';
-import Produtos from './routes/Produtos.jsx';
-import ExcluirProdutos from './routes/ExcluirProdutos.jsx';
+import Home from './router/Home.jsx';
+import Produtos from './router/Produtos.jsx';
+import EditarProdutos from './router/EditarProdutos.jsx';
+import Erro404 from './router/Erro404.jsx';
+import ExcluirProdutos from './router/ExcluirProdutos.jsx';
+import AdicionarProdutos from './router/AdicionarProdutos.jsx';
 
 //Criando o objeto de rotas
 const router = createBrowserRouter([
   { path: '/', element: <App />,
-      errorElement: <Erro404 />, 
-      children: [
-        { path: '/', element: <Home /> },
-        { path: '/produtos', element: <Produtos /> },
-        { path: '/editar/produtos/:id', element: <EditarProdutos /> },
-        { path: '/excluir/produtos/:id', element: <ExcluirProdutos /> },
-      ]
+    errorElement: <Erro404 />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/produtos', element: <Produtos /> },
+      { path: '/editar/produtos/:id', element: <EditarProdutos /> },
+      { path: '/excluir/produtos/:id', element: <ExcluirProdutos /> },
+      { path: '/adicionar/produtos', element: <AdicionarProdutos /> },
+    ]
   },
 ])
 
-//BLOCO DE ROTAS
+
+
+//Bloco de rotas
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
