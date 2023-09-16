de2824915359b134c61a14d45c9aa07f21e3d6af
vittutexx <vitortex06@gmail.com>
2023-09-15 21:07:04 -0300

    atualização da aula do dia 09/15/2023

--- src/router/Produtos.jsx
+++ src/router/Produtos.jsx
@@ -4,15 +4,54 @@ import  styles from "./Produtos.module.css";
 import {AiFillEdit as Editar} from "react-icons/ai";
 import {MdDeleteForever as Excluir} from "react-icons/md";
 import { BiMessageAltAdd as Adicionar } from "react-icons/bi"
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
 
   document.title = "Lista de Produtos";
 
+  const [counter, setCounter] = useState(0);
+  
+  const [counter2, setCounter2] = useState(0);
+
+  
+  useEffect(() => {  
+    
+    console.log("useEffect será renderizado sempre que o componente ou qualquer objeto for atualizado!");
+});
+
+
+
+  useEffect(() => {
+    console.log("useEffect será rendereizado apenas uma vez!");
+},[]);
+
+  const [produtos, setProdutos] = useState([{}]);
+
+
+useEffect(() => {
+  console.log("useEffect será rendereizado apenas uma objeto variável constante que estiver no array de dependências sofrer uma atualização.!");
+  fetch("http://localhost:5000/produtos")
+  .then((lista) => lista.json())
+  .then((listProdutos) => {
+      setProdutos(listProdutos);
+  })  
+
+
+},[counter2]);
+
 
   return (
     <div>
         <h1>Produtos</h1>
+
+        <div>
+          <button onClick={() => setCounter(counter + 1)}>COUNTER - {counter}</button>
+        </div>
+
+        <div>
+          <button onClick={() => setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+        </div>
 
         <table className={styles.table}>
             <thead>
@@ -24,7 +63,7 @@ export default function Produtos() {
             </tr>
             </thead>
             <tbody>
-            {ListaProdutos.map((produto,indice)=>(
+            {produtos.map((produto,indice)=>(
                  <tr key={indice} className={styles.tableRow}>
                     <td className={styles.tableData}>{produto.id}</td>
                     <td className={styles.tableData}>{produto.nome}</td>
