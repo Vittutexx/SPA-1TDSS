f8ac6830b01b916f74c3728530243c3b77070330
vittutexx <vitortex06@gmail.com>
2023-09-22 20:59:19 -0300

    Estrutura do MODAL + controle de transição

--- src/components/ModalInserir/ModalInserir.jsx
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -1,11 +1,30 @@
-export default function ModalInserir(props) {
-    document.title = "CADASTRAR";
+import "./ModalInserir.scss";
 
-    if(props.open){
-    return(
-        <>
-            <h1>ModalInserir</h1>
-        </>
-        );
-    }
+export default function ModalInserir(props) {
+  document.title = "CADASTRAR";
+  if (props.open) {
+    return (
+      <div className="container">
+        <h1>Modal Inserir</h1>
+
+        <div>
+        <button onClick={()=> props.setOpen(false)}>CLOSE - MODAL</button>
+                <form>
+                    <fieldset>
+                        <legend>Cadastrar Produto</legend>
+                        <div>
+                            <label htmlFor="">Nome: </label>
+                            <input type="text" name="nome" placeholder="Digite o nome do produto"/>
+                        </div>
+                        <div>
+                            <label htmlFor="">Preço: </label>
+                            <input type="number" name="preco" placeholder="Digite o valor do produto"/>
+                        </div>
+                    </fieldset>
+                </form>
+        </div>
+        
+      </div>
+    );
+  }
 }
